d2087d1bc33ddf32abf618724b8963a814b954e9
Prathmesh Deval <70071334+Prathmesh-Deval@users.noreply.github.com>
2022-11-24 10:58:25 +0530

    Update reg.py

--- reg.py
+++ reg.py
@@ -5,10 +5,12 @@ text_file = open("Image_to_text.txt", "r")
 # read whole file to a string
 data = text_file.read()
 
+#find pattern in our ID Card
 num = re.search('[A-Z][A-Z][A-Z][A-Z][A-Z][0-9][0-9][0-9][0-9][A-Z]', data)
 s = num.span()
 temp = data[s[0]:s[1]]
 
+#find name in the file
 name = re.search('Name', data)
 r = name.span()
 
@@ -21,14 +23,12 @@ text = data[a:b] + temp
 
 print(text)
 
+#store the found data inside Employee_Data.txt file
 text_output = open('Employee_Data.txt', 'w', encoding='utf-8')
 text_output.write(text)
 text_output.close()
 
-# print(data[int(r[0])+3])
-#
-# x= data[int(r[0]):int(r[0])+20]
-# print(x)
+
 
 
 
